97f366c4a8d68e0d884493a734ec6fc16eff2995
Shnavo <szango297@gmail.com>
2026-07-13 21:53:23 +0200

    feat: added basic commands

--- repl.go
+++ repl.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
-    "fmt"
-    "bufio"
-    "os"
 )
 
 func startRepl() {
@@ -13,18 +13,51 @@ func startRepl() {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 
-        input := cleanInput(scanner.Text())
+		input := cleanInput(scanner.Text())
 		if len(input) == 0 {
 			continue
 		}
-		
+
 		commandName := input[0]
 
-    }
+		command, ok := getCommands()[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		} else {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		}
+	}
 }
+
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	split := strings.Fields(lowered)
 	return split
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display all available commands",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
