859296563cdc8ca30b882797465fd0f28194d879
Shnavo <szango297@gmail.com>
2026-07-13 19:34:44 +0200

    feat: add REPL structure with command input handling

--- repl.go
+++ repl.go
@@ -2,7 +2,26 @@ package main
 
 import (
 	"strings"
+    "fmt"
+    "bufio"
+    "os"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+        input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		
+		commandName := input[0]
+
+    }
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
